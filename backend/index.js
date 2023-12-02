63ba04c5705da1ab7fb24b32c9d8c46d0d99b21c
Raphael Isla <rpbisla0025@gmail.com>
2023-12-02 10:16:07 +0800

    test run

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,6 @@ app.use('/api/orders', order)
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
 })
 .then(() => {
     // listening
